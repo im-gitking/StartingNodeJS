346006abe081af39a1f699b11e7d4e651ec87846
im-gitking <avipal577@gmail.com>
2023-11-13 11:46:58 +0530

    My Third Commit

--- first-app.js
+++ first-app.js
@@ -4,4 +4,3 @@ const fs = require('fs');
 // creating new file named "hello.txt" and writing some text inside it.
 // writeFileSync(fileName, 'content inside it') --> if file not present then create or if present then update 
 fs.writeFileSync('hello.txt', 'Hello fro Node.JS');
-// test
