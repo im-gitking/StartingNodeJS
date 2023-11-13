49a58f45ea5ebf95e77f933aa0a5ec8373d17fac
im-gitking <avipal577@gmail.com>
2023-11-13 13:15:58 +0530

    Product of 2 numbers

--- revise/play.js
+++ revise/play.js
@@ -1,26 +1,5 @@
-const name = 'Max';
-let age = 29;
-const hasHobbies = true;
-
-age = 30;
-
-const summerizeUser = function (userName, userAge, userHobbies) {
-    return ('Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies ' + userHobbies);
+const product = (a, b) => {
+    return a * b;
 }
 
-const summerizeUser2 = (userName, userAge, userHobbies) => {
-    return ('Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies ' + userHobbies);
-}
-
-const add = (a, b) => a + b;
-
-const addOne = a => a + 1;
-
-const addRandom = () => 1 + 10;
-
-console.log(add(2, 3));
-console.log(addOne(2));
-console.log(addRandom());
-
-console.log(summerizeUser(name, age, hasHobbies));
-console.log(summerizeUser2(name, age, hasHobbies));
+console.log(product(2, 3));
