5e2fa42f926b5c6ab7557728cfe0b3ad83837c44
im-gitking <avipal577@gmail.com>
2023-11-14 10:26:04 +0530

    Destructuring

--- revise/arrayIteration.js
+++ revise/arrayIteration.js
@@ -10,4 +10,16 @@ for(let hobby of hobbies) {
 
 const newArr = hobbies.map(hobby => 'Hobby: ' + hobby);
 
-console.log(newArr);
+// can add more values in const array in js
+hobbies.push('Programming');
+console.log(hobbies);
+
+// spread operator -> to copy array and object
+const copiedArr = [...hobbies];
+console.log(copiedArr);
+
+const scndArr = (...args) => {
+    return args;
+}
+
+console.log(scndArr(1,2,3,4));
---
+++ revise/destructing.js
@@ -0,0 +1,23 @@
+const person = {
+    name: 'Max',
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+const printName = ({name, age}) => {
+    console.log(name);
+    console.log(age);
+}
+printName(person);
+//op: Max 29
+
+const {name, age} = person;
+console.log(name, age);
+//op: Max 29
+
+const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+//op: 
